87f301ae19610bc02ab5d3d3e2817507611ebb25
Farah Gustafson <farah.gus@hotmail.com>
2021-07-23 13:40:52 -0400

    completed comments-service

--- src/Comments/comments-service.js
+++ src/Comments/comments-service.js
@@ -1,5 +1,19 @@
 const CommentsService = {
-
+    getAllComment(db) {
+        return db.select('*').from('location_comments')
+    },
+    insertComment(db, newComment) {
+        return db.insert(newComment).into('location_comments').returning('*').then(rows => { return rows[0] })
+    }, 
+    getById(db, id) {
+        return db.from('location_comments').select('*').where('id', id).first()
+    },
+    deleteComment(db, id) {
+        return db('location_comments').where({ id }).delete();
+    },
+    updateComment(db, id, updatedComment) {
+        return db('location_comments').where({ id }).update(updatedComment)
+    },
 }
 
 module.exports = CommentsService
